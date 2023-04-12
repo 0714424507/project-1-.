f10b6aa69694eb0a54fe64711943334baddc0bef
Humphrey <humphrey.muhuri@student.moringaschool.com>
2023-04-12 06:38:16 +0300

    project in progress

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
-let = getAllBeers();
-console.log("beers", beers);
-function getAllBeers() {
-  return fetch("https://api.punkapi.com/v2/beers")
-    .then((res) => res.json())
-    .then((beerData) => singleBeer(beerData));
+function fetchAllBeers() {
+  fetch("https://api.punkapi.com/v2/beers")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      mapBeerList(data);
+    });
 }
-//function to create render one FILM
-function singleBeer(beers) {
-  console.log(beers);
-  let beerList = "";
-  beers.forEach((beer) => {
-    beerList += appendItemOnList(beer);
+
+function mapBeerList(beers) {
+  beers.forEach((element) => {
+    const markup = `<li onclick="beerDetails(${element.id})">${element.name}</li>`;
+    document.getElementById("beers").insertAdjacentHTML("beforeend", markup);
   });
-  document.getElementById("beers").innerHTML = beerList;
 }
-let beer='';
-function appendItemOnList() {
-  let beerList = `<li class="list-beers" onclick="getOneBeer"(${beer.id})"><a >${beer.name} </a></li> `;
-  return beerList;
-}
-function getOneBeer(id) {
-  console.log("beer id", id);
-  let url = "https://api.punkapi.com/v2/beers" + id;
+
+function beerDetails (id){
+  console.log("clicked", id)
+  let url= "https://api.punkapi.com/v2/beers/" + id;
+  console.log(url);
   fetch(url)
-    .then((res) => res.json())
-    .then((beerData) => datForEach(beerData));
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data[0].name);
+    let beerDetails=`<div class="card" style="width: 35rem;">
+    <img src="${data[0].image_url}", class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Name: ${data[0].name}</h5>
+      <h5 class="card-title">Tagline: ${data[0].tagline}</h5>
+      <h5 class="card-title">First Brewed: ${data[0].first_brewed}</h5>
+      <h5 class="card-title">Food Pairing:${data[0].food_pairing}</h5>
+      <h5 class="card-title">Ingridients:${data[0].ingredients.malt[0].name},${data[0].ingredients.malt[1].name}</h5>
+      <h5 class="card-title">Alcohol content:${data[0].abv}</h5>
+      <p class="card-text">Description:${data[0].description}</p>
+    
+    </div>`
+    document.getElementById("beerDetails").innerHTML = beerDetails;
+   
+  });
+
+}
+function searchResults(){
+  
 }
