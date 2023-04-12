8b82e49b9d79eb4750939416e166fa4b02114d23
Humphrey <humphrey.muhuri@student.moringaschool.com>
2023-04-12 08:52:40 +0300

    project in progress

--- index.js
+++ index.js
@@ -8,21 +8,23 @@ function fetchAllBeers() {
 }
 
 function mapBeerList(beers) {
+  //reset page
+ 
   beers.forEach((element) => {
     const markup = `<li onclick="beerDetails(${element.id})">${element.name}</li>`;
     document.getElementById("beers").insertAdjacentHTML("beforeend", markup);
   });
 }
 
-function beerDetails (id){
-  console.log("clicked", id)
-  let url= "https://api.punkapi.com/v2/beers/" + id;
+function beerDetails(id) {
+  console.log("clicked", id);
+  let url = "https://api.punkapi.com/v2/beers/" + id;
   console.log(url);
   fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data[0].name);
-    let beerDetails=`<div class="card" style="width: 35rem;">
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data[0].name);
+      let beerDetails = `<div class="card" style="width: 35rem;">
     <img src="${data[0].image_url}", class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">Name: ${data[0].name}</h5>
@@ -33,21 +35,19 @@ function beerDetails (id){
       <h5 class="card-title">Alcohol content:${data[0].abv}</h5>
       <p class="card-text">Description:${data[0].description}</p>
     
-    </div>`
-    document.getElementById("beerDetails").innerHTML = beerDetails;
-   
-  });
-
+    </div>`;
+      document.getElementById("beerDetails").innerHTML = beerDetails;
+    });
 }
-function searchResults(form){
+
+searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("search form clicked", form)
-  let keyword= form.element.keyword.value;
-  console.log("keyword value", keyword)
-  fetch("https://api.punkapi.com/v2/beers?beer_name=" + keyword)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  })
-
-}
+  let keyword = document.getElementById("keyword");
+  console.log("keyword", keyword.value);
+  fetch("https://api.punkapi.com/v2/beers?beer_name=" + keyword.value)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      mapBeerList(data);
+    });
+});
